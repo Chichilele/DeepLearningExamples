41f2fa2da2241afedfd0e9a0472f8721eafbca5f
G <gauthier.castro@cfms.org.uk>
2021-01-29 16:00:04 +0000

    log to file

--- PyTorch/Detection/SSD/examples/eval_FTI.sh
+++ PyTorch/Detection/SSD/examples/eval_FTI.sh
@@ -1,8 +1,9 @@
-echo $1
+echo 'folder' $1
+echo 'writing to file' $1/../eval.txt
 for item in $(ls $1/*)
 do
   echo $item
-  python main.py  --backbone resnet50 --amp --ebs 32 --data /coco --mode evaluation --checkpoint $item
+  python main.py  --backbone resnet50 --amp --ebs 32 --data /coco --mode evaluation --checkpoint $item >> $1/../eval.txt
 done
 echo 'done!'
 
