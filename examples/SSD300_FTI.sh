ac55a4ab134d49809b0fdc1867f6343a4a4149f6
G <gauthier.castro@cfms.org.uk>
2021-01-29 17:00:54 +0000

    no save run

--- examples/SSD300_FTI.sh
+++ examples/SSD300_FTI.sh
@@ -1,3 +1,3 @@
-python ./main.py --backbone resnet50 --warmup 10 --bs 64 --epochs 50 --multistep 25 35  \
-    --evaluation 0 1 5 10 15 20 25 30 35 40 45 50 \
-    --amp --data /coco --save /coco/experiments/2labels_512/models --json-summary /coco/experiments/2labels_512/summary.json
+python ./main.py --backbone resnet50 --warmup 300 --bs 8 --epochs 50 --multistep 25 35  \
+    --evaluation 0 1 2\
+    --amp --data /coco --json-summary ./summary.json
