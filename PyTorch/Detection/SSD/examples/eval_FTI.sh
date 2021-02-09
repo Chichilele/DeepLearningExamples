817f9c02f9cd4a6a9c84160a69c4afa6a019ebce
G <gauthier.castro@cfms.org.uk>
2021-02-09 11:30:23 +0000

    parent folder and print to file

--- PyTorch/Detection/SSD/examples/eval_FTI.sh
+++ PyTorch/Detection/SSD/examples/eval_FTI.sh
@@ -1,8 +1,9 @@
-echo $1
-for item in $(ls $1/*)
+echo 'folder' $1
+WORK_DIR=$(readlink -f $1/../)
+echo 'work dir' $WORK_DIR
+for item in $(ls $WORK_DIR/models/*)
 do
   echo $item
-  python main.py  --backbone resnet50 --amp --ebs 32 --data /coco --mode evaluation --checkpoint $item
+  python main.py  --backbone resnet50 --amp --ebs 8 --data /coco --mode evaluation --checkpoint $item >> $WORK_DIR/eval.txt
 done
 echo 'done!'
-
