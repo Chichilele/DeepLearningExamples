cbce6b4d3db2fc874756a7910385aefb298b58d5
G <gauthier.castro@cfms.org.uk>
2021-02-10 13:54:46 +0000

    update eval_FTI.sh

--- examples/eval_FTI.sh
+++ examples/eval_FTI.sh
@@ -1,9 +1,10 @@
 echo 'folder' $1
-echo 'writing to file' $1/../eval.txt
-for item in $(ls $1/*)
+WORK_DIR=$(readlink -f $1/../)
+echo 'work dir' $WORK_DIR
+for item in $(ls $WORK_DIR/models/*)
 do
   echo $item
-  python main.py  --backbone resnet50 --amp --ebs 32 --data /coco --mode evaluation --checkpoint $item >> $1/../eval.txt
+  echo $item
+  python main.py  --backbone resnet50 --amp --ebs 8 --data /coco --mode evaluation --checkpoint $item >> $WORK_DIR/eval.txt
 done
 echo 'done!'
-
